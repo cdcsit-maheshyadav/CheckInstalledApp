c004b32d9c4302c79b1e9d28063c59902940dae3
mahesh <mahesh@MAHESH>
2022-08-24 11:14:46 +0545

    Add project files.

---
+++ CheckInstalledApp/InstalledApp.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using Microsoft.Win32;
+
+namespace CheckInstalledApp
+{
+    public class InstalledApp
+    {
+        /// 08/04/2022 Mahesh Kumar Yadav. <br/>
+        /// <summary>
+        /// Check app is installed or not
+        /// </summary>
+        /// <param name="cName">Application name</param>
+        /// <returns></returns>
+        public static bool IsAppInstalled(string cName)
+        {
+            string displayName;
+
+            var registryKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            var key = Registry.LocalMachine.OpenSubKey(registryKey);
+            if (key != null)
+            {
+                foreach (var subKey in key.GetSubKeyNames().Select(keyName => key.OpenSubKey(keyName)))
+                {
+                    displayName = subKey?.GetValue("DisplayName") as string;
+
+                    if (displayName != null && displayName.Contains(cName))
+                    {
+                        return true;
+                    }
+                }
+
+                key.Close();
+            }
+
+            registryKey = @"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall";
+            key = Registry.LocalMachine.OpenSubKey(registryKey);
+            if (key == null) return false;
+            {
+                foreach (var subKey in key.GetSubKeyNames().Select(keyName => key.OpenSubKey(keyName)))
+                {
+                    displayName = subKey?.GetValue("DisplayName") as string;
+                    if (displayName != null && displayName.Contains(cName))
+                    {
+                        return true;
+                    }
+                }
+
+                key.Close();
+            }
+            return false;
+        }
+    }
+}
---
+++ CheckInstalledApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckInstalledApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of application.");
+            var appName=Console.Read().ToString();
+            var app = InstalledApp.IsAppInstalled(appName);
+
+            Console.WriteLine(app);
+            Console.Read();
+        }
+    }
+}
